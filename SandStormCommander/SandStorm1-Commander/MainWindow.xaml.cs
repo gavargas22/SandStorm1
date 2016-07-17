42847f9498c3e64de6a0e3768fcd528e0553154d
Guillermo Vargas <gavargas22@gmail.com>
2016-07-17 01:47:09 -0600

    Started C# Commander

---
+++ SandStormCommander/SandStorm1-Commander/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SandStorm1_Commander
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int baudRate = 115200;
+        public string comPortName;
+        SerialPort sandStormSerialPort = new SerialPort();
+        public bool continueReadingSerialPort = true;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Get all the ports available.
+            string[] serialPortsAvailable = SerialPort.GetPortNames();
+            //List all of the serial ports to initiate a connection
+            serialPortsList.ItemsSource = serialPortsAvailable;
+            
+        }
+
+        // A function to connect to the Serial port using the parameters desired.
+        public void connectToSerialPort(string port)
+        {
+            // Connect with the parameters.
+            sandStormSerialPort.PortName = comPortName;
+            sandStormSerialPort.BaudRate = baudRate;
+            sandStormSerialPort.Open();
+
+            comPortConnectButton.IsEnabled = false;
+            comPortCloseButton.IsEnabled = true;
+
+            while (continueReadingSerialPort == true)
+            {
+                string data = sandStormSerialPort.ReadLine();
+                Console.WriteLine(data);
+            }
+        }
+
+        private void connectToSelectedComPort(object sender, RoutedEventArgs e)
+        {
+            //Console.WriteLine("Setting the Port and connecting...");
+            connectToSerialPort(comPortName);
+        }
+
+        private void serialPortsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            comPortName = serialPortsList.SelectedValue.ToString();
+            Console.WriteLine(comPortName);
+
+        }
+
+        private void closeSelectedComPort(object sender, RoutedEventArgs e)
+        {
+            sandStormSerialPort.Close();
+            comPortConnectButton.IsEnabled = true;
+            this.IsEnabled = false;
+        }
+
+
+        
+    }
+
+    
+}
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,5 +1,6 @@
 #include <pt.h>   // include protothread library
 #include <SoftwareSerial.h>
+#include "IntersemaBaro.h"
 
 #define RXPin 52
 #define TXPin 53
@@ -8,61 +9,66 @@
 #define GPS_BAUDRATE 9600
 #define NMEA_SENTENCE_MAX 300
 
-static struct pt gps, heartbeat, sensors; // each protothread needs one of these
+static struct pt, gps, heartbeat, sensors; // each protothread needs one of these
 char nmea_sentence[NMEA_SENTENCE_MAX];
 int counter = 0;
+// Declare software serial variables.
 SoftwareSerial gpsSerial(RXPin, TXPin);
+// Altimeter
+Intersema::BaroPressure_MS5607B baro(true);
+
 
 void setup() {
   Serial.begin(BAUDRATE);
   Serial1.begin(BAUDRATE);
-  
+
   gpsSerial.begin(GPS_BAUDRATE);
-  
+  // Barometer system
+  baro.init();
+
   PT_INIT(&gps);  // initialise the three // protothread variables
   PT_INIT(&heartbeat);
-  PT_INIT(&sensors);  
+  PT_INIT(&sensors);
 }
 
-PT_THREAD(verifyHeartbeat(struct pt *pt, int interval))
-{
+PT_THREAD(verifyHeartbeat(struct pt *pt, int interval)) {
   static char tick[4];
   static int i = 0;
   static String message = "";
   char currentChar;
-  
+
   static unsigned long timestamp = 0;
-  
+
   PT_BEGIN(pt);
-  
+
   Serial.println("Heart!");
-  
+
   while(1)
   {
-    
+
     PT_WAIT_UNTIL(pt, millis() - timestamp > interval);
-    
+
     timestamp = millis();
-   
+
    if(Serial.available() > 3)
    {
         i = 0;
    }
-   
+
    for(int j = 0; j < Serial.available(); j++)
    {
       currentChar = Serial.read();
       Serial.println(currentChar);
-        
+
       if( i > 3 ) { i = 0; }
-      
+
       Serial.println(i,DEC);
       tick[i] = currentChar;
       i++;
       tick[i] = '\0';
       message = tick;
     }
-    
+
     if(message.equals("tick"))
     {
       Serial.println("tock");
@@ -70,7 +76,7 @@ PT_THREAD(verifyHeartbeat(struct pt *pt, int interval))
       i = 0;
     }
   }
-  
+
   PT_END(pt);
 }
 
@@ -78,29 +84,29 @@ PT_THREAD(verifyHeartbeat(struct pt *pt, int interval))
 static int transmitGPS(struct pt *pt, int interval) {
   static unsigned long timestamp = 0;
   char byteGPS;
-  
+
   PT_BEGIN(pt);
-  
-  while(1) 
+
+  while(1)
   {
-    
+
     while(1) {
       PT_WAIT_UNTIL(pt, millis() - timestamp > interval);
        timestamp = millis();
-       
+
       if (gpsSerial.available())
       {
          byteGPS = gpsSerial.read();
          handle_byte(byteGPS);
       }
-         
+
 //      if (Serial.available())
 //        gpsSerial.write(Serial.read());
-        
+
       //Serial.println("GPS");
     }
   }
-  
+
   PT_END(pt);
 }
 
@@ -111,42 +117,44 @@ int handle_byte(int byteGPS)
     Serial.println(nmea_sentence);
     counter = 0;
   }
-  
+
   if(counter == 300)
     counter = 0;
-  
+
   nmea_sentence[counter] = byteGPS;
   counter++;
 }
 
-
+// Update for reporting altimeter
 PT_THREAD(updateSensorInfo(struct pt *pt, int interval)) {
   static unsigned long timestamp = 0;
   struct pt heartbeat;
-  
+
   PT_BEGIN(pt);
-  
-  while(1) 
+
+  long alt_in_cm = baro.getHeightCentimeters();
+  float al_in_feet = ((float)(alt_in_cm) / 30.48);
+
+  while(1)
   {
-    
+
     PT_WAIT_UNTIL(pt, millis() - timestamp > interval);
-    
+
     Serial.println("Starting");
-    
+
     timestamp = millis();
-    
+
     Serial.println("UPDATE SENSOR");
- 
+
     Serial.print("timestamp: ");
     Serial.println(timestamp);
-  
+
   }
-  
+
   PT_END(pt);
 }
 
-void loop() 
-{
+void loop() {
   transmitGPS(&gps, 5); // by calling them infinitely
   updateSensorInfo(&sensors, 3000);
   verifyHeartbeat(&heartbeat, 1000);
